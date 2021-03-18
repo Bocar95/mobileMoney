05cac5c6a4874f4289554a213df8d02f97adaaf7
Bocar95 <bocar.diallo95@gmail.com>
2021-03-18 00:18:33 +0000

    lid Transaction

--- frontEndMobileMoney/src/app/list-transactions/list-transactions.component.ts
+++ frontEndMobileMoney/src/app/list-transactions/list-transactions.component.ts
@@ -12,13 +12,7 @@ export class ListTransactionsComponent implements OnInit {
 
   tab : {}[];
   tab2 : any;
-
   username : number;
-  trans;
-  userDepot;
-  userRetrait;
-  prenom;
-  nom;
 
   constructor(private router : Router, private transationService : TransactionService, private userService : UserService) { }
 
@@ -31,29 +25,7 @@ export class ListTransactionsComponent implements OnInit {
     return this.router.navigate(['/acceuil']);
   }
 
-  tokenElement() {
-    let token = localStorage.getItem('token');
-    var base64Url = token.split('.')[1];
-    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
-
-    var element = JSON.parse(jsonPayload);
-    this.username = element["username"];
-    return element;
-  }
-
-  getUser(){
-    return this.transationService.getUserByUsername(this.username).subscribe(
-      res => {
-        console.log(res),
-        this.fusionOp(res["id"])
-      }
-    );
-  }
-
-  fusionOp(id){
+  getTrans(id){
     return this.transationService.fusion(id).subscribe({
       next: (res) => {
         this.tab2 = res,
@@ -68,6 +40,28 @@ export class ListTransactionsComponent implements OnInit {
       },
       error: (err) => {}
     });
+  }
+
+  getUser(){
+    return this.transationService.getUserByUsername(this.username).subscribe(
+      res => {
+        console.log(res),
+        this.getTrans(res["id"])
+      }
+    );
+  }
+
+  tokenElement() {
+    let token = localStorage.getItem('token');
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    var element = JSON.parse(jsonPayload);
+    this.username = element["username"];
+    return element;
   }
   
 }
